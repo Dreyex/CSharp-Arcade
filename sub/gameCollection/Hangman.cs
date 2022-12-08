29eda89e3d49db2ce5ae107e1f3af604a34c7898
Dreyex <mr.max.f@gmx.de>
2022-12-08 10:33:15 +0100

    Hangman Completion

--- sub/gameCollection/Hangman.cs
+++ sub/gameCollection/Hangman.cs
@@ -23,6 +23,13 @@ public class Hangman
         {"   ██████   ██████   ██████  ██████  ██████     ██    ███████  "}, 
     };
 
+    int guessedWrong = 0; //maximum 6
+    String wrongLetters = "";
+    String word;
+    String hidden;
+    char[] wordA;          //word as Char-Array 
+    char[] hiddenA;     //hidden word as Char-Array 
+    String guess;
 
 
     //methods
@@ -92,32 +99,86 @@ public class Hangman
     }
 
     private void game()
-    {
-        int guessedWrong = 0;
-        String wrongLetters = "";
-        String word = wordChooser();
-        String hidden = wordHider(word);
-        //Console.WriteLine(word);
-        //Console.WriteLine(hidden);
-        char[] wordA = word.ToCharArray();          //word as Char-Array 
-        char[] hiddenA = hidden.ToCharArray();     //hidden word as Char-Array 
+    {   
+        //get a value for the attributes
+        word = wordChooser().ToUpper();               //get a word to guess
+        hidden = wordHider(word);           //hide the word from the screen
+        wordA = word.ToCharArray();         //convert word to array
+        hiddenA = hidden.ToCharArray();     //convert hidden to array
 
-        String[,] man = new String[,]           //Hangman 
-        { 
-            
-        };
+        while(!wordA.Equals(hiddenA) && guessedWrong != 8)
+        {
+            Console.WriteLine("----------------------------------------");
+            hangman(guessedWrong);  //displays the current state of the Hangman
+            Console.WriteLine("Wrong Letters: " + wrongLetters);
+            Console.WriteLine("Word to guess: ");
+            foreach(char i in hiddenA)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Please input a letter: ");
+            ConsoleKeyInfo key = Console.ReadKey(); //gets the pressed key
+            char input = key.KeyChar;               //converts it to a char
+            input = char.ToUpper(input); //converts it to Uppercase 
+            Console.WriteLine();               
+            for(int i = 0; i < wordA.Length; i++)
+            {
+                if(input == wordA[i])
+                {
+                    hiddenA[i] = input; 
+                }
+                else if(wrongLetters.Contains(input))
+                {
+                    break;
+                }
+                else if(i == wordA.Length - 1)
+                {  
+                    guessedWrong++;
+                    wrongLetters += input;
+                }
+            }
 
 
+        }
+        if(guessedWrong == 8)
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("----------------------------------------");
+            hangman(guessedWrong);
+            Console.WriteLine("Game Over"); 
+            Console.WriteLine("The word was: " + word);      
+            Console.WriteLine("Guessed Wrong: " + guessedWrong);
+            Console.WriteLine("Wrong guessed Letters: " + wrongLetters);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("----------------------------------------");
+        }
+        else if(wordA.Equals(hiddenA))
+        {
+             Console.WriteLine("----------------------------------------");
+             Console.WriteLine("----------------------------------------");
+             Console.WriteLine("----------------------------------------");
+             Console.WriteLine("You Won!"); 
+             Console.WriteLine("The word was: " + word);      
+             Console.WriteLine("Guessed Wrong: " + guessedWrong);
+             Console.WriteLine("Wroong guessed Letters: " + wrongLetters);
+             Console.WriteLine("----------------------------------------");
+             Console.WriteLine("----------------------------------------");
+             Console.WriteLine("----------------------------------------");
+        }
     }
 
-    private String wordChooser()
+    private String wordChooser()    //chooeses a random word rom a very big list of words
     {
-        String[] lines = File.ReadAllLines("C:\\Users\\user802\\Documents\\Cyberspace\\CSharp-Arcade\\sub\\gameCollection\\wordList\\wordList.txt");
-        Random rand = new Random();
-        return lines[rand.Next(0, lines.Length - 1)];
+        String[] lines = File.ReadAllLines("C:\\Users\\user802\\Documents\\Cyberspace\\CSharp-Arcade\\sub\\gameCollection\\wordList\\wordList.txt");    //reads every line of a file and for each line it saves the value in an array
+        Random rand = new Random();                         //get a random number
+        return lines[rand.Next(0, lines.Length - 1)];       //from in the given dimensions
     }
 
-    private String wordHider(String word)
+    private String wordHider(String word)       //hides word with "_" to display the word 
     {
         String hidden = "";
         for(int i = 0; i < word.Length; i++)
@@ -125,6 +186,135 @@ public class Hangman
             hidden += "_";
         }
         return hidden;
+    }
+
+    private void hangman(int guessesWrong)      //displays the hangman based on the wrong guessed letters
+    {
+        if (guessesWrong == 0)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 1)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 2)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 3)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 4)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   ________");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 5)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   ________");
+            Console.WriteLine("   |      |");
+            Console.WriteLine("   |      O");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 6)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   ________");
+            Console.WriteLine("   |      |");
+            Console.WriteLine("   |      O");
+            Console.WriteLine("   |      |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 7)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   ________");
+            Console.WriteLine("   |      |");
+            Console.WriteLine("   |      O");
+            Console.WriteLine("   |     /|\\");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+        else if (guessesWrong == 8)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   ________");
+            Console.WriteLine("   |      |");
+            Console.WriteLine("   |      O");
+            Console.WriteLine("   |     /|\\");
+            Console.WriteLine("   |      |");
+            Console.WriteLine("   |     / \\ ");
+            Console.WriteLine("   |_______");
+            Console.WriteLine("   |       \\");
+            Console.WriteLine("   |________|");
+            Console.WriteLine("");
+        }
+
+       
+
     }
 
 }
